0d02df53e92e33823d82771c34588d5eeed73ac0
whydandrian <whydandrian@gmail.com>
2021-07-04 22:48:53 +0700

    add file header blade

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,37 @@
+<div class="card text-dark bg-transparent mt-5 border-0">
+  <div class="card-body">
+    <div class="row">
+      <div class="col">
+        <h1 class="align-self-start mt-5 fs-1 fw-bolder">
+          Ruangguru Super App:
+        </h1>
+        <h1 class="align-self-start mt-3 fs-1 fw-bolder">Belajar <span id="typed" class="typed"></span></h1>
+        <h5 class="align-self-start mt-3 fw-bolder">
+          Aplikasi Bimbingan Belajar Online Terbaik dan Terbesar di Indonesia
+        </h5>
+      </div>
+      <div class="col">
+        <img src="https://www.ruangguru.com/hubfs/1%20Homepage%20RG/Optimize/asset_heroillust%20(2)%20(1).png" alt="">
+      </div>
+    </div>
+    <div class="row my-3">
+      <div class="col-md">
+        <h4 class="fw-bold">22.000.000+</h4>
+        <h6>Pengguna Ruangguru</h6>
+      </div>
+      <div class="col-md">
+        <h4 class="fw-bold">300.000+</h4>
+        <h6>Pengajar di Ruangguru</h6>
+      </div>
+      <div class="col-md">
+        <h4 class="fw-bold">69%</h4>
+        <h6>Pengguna Ruangguru diterima PTN</h6>
+      </div>
+      <div class="col-md">
+        <h4 class="fw-bold">9.3/10</h4>
+        <h6>Tingkat kepuasan Pengguna Ruangguru</h6>
+      </div>
+      
+    </div>
+  </div>
+</div>
